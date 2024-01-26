efc7e52b397f40c7d9e50ee9797f9d1dfd1bab94
nhattx <narusunday@gmail.com>
2024-01-26 14:56:43 +0700

    init2

--- polls/forms.py
+++ polls/forms.py
@@ -2,9 +2,9 @@ from django import forms
 
 
 class DemoLockMeetingForm(forms.Form):
-    registration_host = forms.CharField(required=True, label="Registration Host")
-    alias = forms.CharField(required=True, label="Alias")
-    username = forms.CharField(required=True, label="Username")
-    password = forms.CharField(required=True, label="Password")
-    meeting_name = forms.CharField(required=True, label="Meeting Name")
-    meeting_host_code = forms.IntegerField(required=True, label="Meeting Code")
+    registration_host = forms.CharField(required=True, label="Máy chủ")
+    alias = forms.CharField(required=True, label="Tên Định Danh")
+    username = forms.CharField(required=True, label="Tên Tài Khoản")
+    password = forms.CharField(required=True, label="Mật Khẩu")
+    meeting_name = forms.CharField(required=True, label="Tên Hội Nghị")
+    meeting_host_code = forms.IntegerField(required=True, label="Mã Hội Nghị")
